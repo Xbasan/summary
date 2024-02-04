2213035c6fe75be4e1f7647c8cf2318796ddc138
Xbasan <121888389+Xbasan@users.noreply.github.com>
2024-02-04 17:19:58 +0300

    резюме

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+contact = '''        
+<ul class="nav-wrapper">
+            <li class="contact">
+                <a href="https://t.me/Xbasan">
+                    <img class=img-icon src="static/img/tg.svg">
+                </a>
+            </li>
+            <li class="contact">
+                <a href="https://github.com/Xbasan">
+                    <img class=img-icon src="static/img/git.svg">
+                </a>
+            </li>
+            <li class="contact">
+                <a href="mailto:xamzpok@gmail.com">
+                    <img class=img-icon src="static/img/gmail.svg">
+                </a>
+            </li>
+        </ul>'''
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('index.html', contact=contact)
+
+@app.route('/projects', methods=['GET'])
+def projects():
+    return render_template('projects.html', contact=contact)
+
+if __name__ == '__main__':
+    app.run(debug=True,
+              host='0.0.0.0') # 6969
---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.btn_order').addEventListener('click', ()=>{
+    document.getElementById('app').style = 'display: flex;animation: appearAnimation 1s ease;'
+})
+
+document.querySelector('.btn-close').addEventListener('click', ()=>{
+    document.getElementById('app').style.display='none'
+})
